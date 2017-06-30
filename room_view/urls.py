b6991ce3e2e4277142f3b4e27d071196f89891d6
garyyb <garyybai@gmail.com>
2017-06-30 20:42:23 +1000

    server static files

--- room_view/urls.py
+++ room_view/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import controllers
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', controllers.index),
-]
+    url(r'^$', controllers.index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
